b978c327af40bbb2b55660a972be944a47e657e9
Md. Shahriar Kabir <shahriar1904@cseku.ac.bd>
2022-01-05 14:26:44 +0600

    added sidebar

--- pages/houses/[id].jsx
+++ pages/houses/[id].jsx
@@ -5,15 +5,34 @@ function House({ house }) {
     var { id, picture, type, town, title, description, guests } = house
     return (
         <Layout content={<div>
-            <Head>
-                <title> {title} </title>
-            </Head>
             <img src={picture} width="100%" alt="House picture" />
-            <p>
-                {type} - {town}
-            </p>
-            <p>{description}</p>
-            <p>{guests}</p>
+
+            <div className="container">
+                <Head>
+                    <title> {title} </title>
+                </Head>
+                <article>
+                    <p>
+                        {type} - {town}
+                    </p>
+                    <p>{description}</p>
+                    <p>{guests}</p>
+                </article>
+                <aside>xnxx</aside>
+            </div>
+            <style jsx>{`
+            .container{
+                display: grid;
+                grid-template-columns: 55% 40%;
+                grid-gap: 5%
+            }
+            aside{
+                border: 1px solid;
+                border-radius: 5px;
+                padding: 1em;
+                box-shadow: 2px 2px 1px 1px;
+            }
+            ` }</style>
 
         </div>} />
 
